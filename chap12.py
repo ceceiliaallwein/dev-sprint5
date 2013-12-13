2961de55e2c9873b56ce4e89821e1298a2fbacb9
ceceiliaallwein <ceceilia.allwein@gmail.com>
2013-12-12 20:57:26 -0500

    class commits

--- chap12.py
+++ chap12.py
@@ -16,12 +16,17 @@ Here is an example of what the output might look like:
 ['resmelts', 'smelters', 'termless']
 
 Hint: you might want to build a dictionary that maps from a set of letters to a list of words that can be spelled with those letters. The question is, how can you represent the set of letters in a way that can be used as a key?
-
-
-2
-Modify the previous program so that it prints the largest set of anagrams first, followed by the second largest set, and so on.
-
-
-3 In Scrabble a “bingo” is when you play all seven tiles in your rack, along with a letter on the board, to form an eight-letter word. What set of 8 letters forms the most possible bingos? Hint: there are seven. 
-
 '''
+
+def read_file(filename): 
+	storage = dict()
+	with open(filename) as fin: 
+		for line in fin: 
+			line = line.strip()
+			storage[line] = storage.get(line,0) + 1
+		return storage
+
+print read_file('words.txt')
+
+
+
--- chap13_notes.py
+++ chap13_notes.py
@@ -24,7 +24,61 @@ def total_words(histogram):
 def different_words(histogram):
 	return len(histogram)
 
-h = process_file('emma.txt')
-t = total_words(h)
-print h
-print t
+# h = process_file('emma.txt')
+# t = total_words(h)
+# print 'Total number of words: ' + str(len(h))
+# print 'Number of different words: ' + str(t)
+
+
+def most_common(histogram):
+	t = []
+	for key, value in hist.items():
+		t.append((value,key))
+
+	t.sort(reverse=True)
+	return t
+
+
+
+#Exercise 13.5
+
+def histogram(s): 
+	d = dict()
+	for c in s: 
+		if c not in d: 
+			d[c] = 1
+		else: 
+			d[c] += 1
+	return d 
+
+
+def print_hist(h): 
+	output = dict()
+	for c in h:
+		print c, h[c]
+
+import random
+print histogram('ceceilia')
+
+def choose_rand(arg):
+	d = histogram(arg)
+	return random.choice(list(d.keys()))
+	
+def choose_weighted(arg):
+	d = histogram(arg)
+
+# http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice?lq=1
+
+
+print choose('ceceilia')
+
+
+
+
+# h = histogram('ceceilia')
+# print h.get('a',0)
+# print_hist(h)
+
+
+
+
